96339d72031529400cf941c0606d43bc1acb8e27
1andonlyziz <T3zEez512@gmail.com>
2022-01-24 18:19:25 +0300

    Uploading Classification Algorithem

---
+++ MachineLearning/app.py
@@ -0,0 +1,39 @@
+# Dependencies
+import sys
+from flask import Flask, request, jsonify
+import joblib
+import traceback
+import pandas as pd
+import model
+
+# Your API definition
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if clf:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(clf.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
+
+if __name__ == '__main__':
+
+    port = 5000
+    clf = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
+    app.run(port=port, debug=True)
