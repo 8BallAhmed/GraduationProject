76c05f7a46369be16ba21e3f754745f344a9b4ce
8BallAhmed <ahmed.fcit88@gmail.com>
2022-02-07 23:26:48 +0300

    fix: moved api endpoints to separate files

--- Backend/express-app.js
+++ Backend/express-app.js
@@ -8,27 +8,6 @@ app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
 });
 
-app.get("/getPatients", (req, res) => {
-  const header = req.header;
-  if (header == undefined) {
-    res.end(
-      JSON.stringify({
-        status: 400,
-        message: "Authentication Header not specified!",
-      })
-    );
-  } else {
-    let patients = Patient.findAll().then((result) => {
-      console.log(result);
-      res.end(
-        JSON.stringify({
-          status: 200,
-          message: "Query successful",
-          patients: result,
-        })
-      );
-    });
-  }
-});
+module.exports.app = app;
 
 // Take care of JWT Auth in Middleware
