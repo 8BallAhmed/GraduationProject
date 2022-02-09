a9a3afc980fe5ca462a7e770a8c2cb1d5e013cd0
8BallAhmed <ahmed.fcit88@gmail.com>
2022-02-09 22:51:39 +0300

    chore: added proper validation for email, pwd and account_type

--- Backend/joi-validators.js
+++ Backend/joi-validators.js
@@ -1,15 +1,19 @@
 const joi = require("joi");
 
 const registerSchema = joi.object({
-  email: joi.exist(),
-  password: joi.exist(),
+  email: joi.string().email({
+    tlds: {
+      allow: true,
+    },
+  }),
+  password: joi.string().min(8).alphanum(),
   address: joi.exist(),
   name: joi.exist(),
   ssn: joi.exist(),
   gender: joi.exist(),
   city: joi.exist(),
   dob: joi.exist(),
-  account_type: joi.exist(),
+  account_type: joi.string().required().equal("patient", "doctor"),
 });
 
 module.exports.registerSchema = registerSchema;
