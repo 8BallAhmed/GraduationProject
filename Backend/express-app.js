dca514a44f74737b94ed6a7149701fe7dfb9b317
8BallAhmed <ahmed.fcit88@gmail.com>
2022-02-17 00:40:46 +0300

    fix: fixed environment file path

--- Backend/express-app.js
+++ Backend/express-app.js
@@ -1,8 +1,12 @@
 require("dotenv").config({ path: "./.env" }); // Set running DIR
 const model = require("./model");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
 });
