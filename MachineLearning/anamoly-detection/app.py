bad25b7d8c647f222e2f975259d96c9dd1290b62
1andonlyziz <73785385+1andonlyziz@users.noreply.github.com>
2022-02-20 01:38:14 +0300

    feat:completed anamoly detection endpoint

--- MachineLearning/anamoly-detection/app.py
+++ MachineLearning/anamoly-detection/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, request,abort
 import pandas as pd
+import numpy as np
+from sklearn import svm
+import traceback
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-
-import sqlalchemy
-from sqlalchemy import create_engine,text
-
+from sqlalchemy import create_engine
 
 
 dotenv_path = Path('././.env')
@@ -15,16 +15,21 @@ load_dotenv(dotenv_path=dotenv_path)
 username = os.environ['DBUSER']
 password = os.environ['DBPASSWORD']
 
-engine = create_engine('postgresql://{username}:{password}@localhost:5432/glucoguardian'.format(username=username,password=password))
+engine = create_engine(
+    'postgresql://{username}:{password}@localhost:5432/glucoguardian'.format(username=username, password=password))
 
-with engine.connect() as con:
-    rs = con.execute('SELECT * FROM glucose_test')
+
+def fetch_glucose_test(patient_id):
+    with engine.connect() as con:
+        rs = con.execute('SELECT glucose_level FROM glucose_test where patient_id={patient_id} and anamoly=False'.format(
+            patient_id=patient_id))
+        x = list()
     for row in rs:
-        print(row)
-        
-# app = Flask(__app__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/glucoguardian'
-# db=SQLAlchemy(app)
+        x.append(row[0])
+    return np.array(x)
+
+
+app = Flask(__app__)
 
 '''
 anamoly detection endpoint
@@ -33,13 +38,45 @@ anamoly detection endpoint
 3- within the endpoint classify if the glucose_test was before or after food for better model accuracy
 4- if anamoly was detected set the anamoly attribute to true : else false
 '''
-# @app.route('/detect-anamoly',methods=['POST'])
-# def anamoly_detection():
-#     try :
-        
-#         body = request.get_json()
-#         patient_id = body.get('patient_id')
-#     except :
-#         print('hello')
 
-#     return Response('Hellow')
+@app.route('/hello',methods=['GET'])
+def test():
+    return jsonify({'status':200})
+
+@app.route('/detect-anamoly', methods=['POST'])
+def anamoly_detection():
+    try:
+        # fetching data from body
+        body = request.get_json()
+        patient_id = body.get('patient_id')
+        glucose_level = body.get('glucose_level')
+       
+        # blood_pressure = body.get('blood_pressure')
+        # pills = body.get('pills')
+        # activity = body.get('activity')
+        # meal = body.get('meal')
+        # time_interval = body.get('time_interval')
+        # meal = body.get('time')
+        
+    except:
+        print('error fetching records from the endpoint')
+        return jsonify({'trace': traceback.format_exc()})
+    try:
+        # fetching data from database
+        X_train = fetch_glucose_test(patient_id)
+    except:
+        print('error fetching records from database')
+        return jsonify({'trace': traceback.format_exc()})
+    try:
+        # fit the model and detect anamoly
+        clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+        clf.fit(X_train)
+        y_pred = clf.predict(glucose_level)
+    except:
+        print('error while detecting an anamoly')
+        return jsonify({'trace': traceback.format_exc()})
+    return jsonify({
+        'status': 200,
+        'message': 'success',
+        'result': y_pred
+        })
