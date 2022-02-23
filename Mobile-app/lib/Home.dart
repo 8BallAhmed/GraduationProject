ffebae9eb2a991c3831a9ee90bab45b6f709fb6c
Mubarak <brook.aloufi@gmail.com>
2022-02-24 02:56:23 +0300

    feat: complete the home page

--- Mobile-app/lib/Home.dart
+++ Mobile-app/lib/Home.dart
@@ -117,8 +117,186 @@ class _HomeState extends State<Home> {
                   SizedBox(
                     height: 10,
                   ),
+                  Flexible(
+                    flex: 1,
+                    fit: FlexFit.loose,
+                    child: Container(
+                      width: 380,
+                      height: 151,
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color(0xFF101E73).withOpacity(0.08),
+                                spreadRadius: 5,
+                                blurRadius: 6,
+                                offset: Offset(1.0, 2.0))
+                          ],
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          width: double.infinity,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.symmetric(vertical: 15),
+                                    child: Text(
+                                      "1092 Calories",
+                                      style: TextStyle(fontSize: 27),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text("Fat",
+                                          style: TextStyle(fontSize: 27)),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text("13G",
+                                          style: TextStyle(fontSize: 27))
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text("Carbs",
+                                          style: TextStyle(fontSize: 27)),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text("67G",
+                                          style: TextStyle(fontSize: 27))
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text("Protein",
+                                          style: TextStyle(fontSize: 27)),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text("45G",
+                                          style: TextStyle(fontSize: 27))
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: check == 1 ? active : notActive,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Visibility(
+                            visible: vis,
+                            child: RawMaterialButton(
+                              onPressed: () {},
+                              elevation: 2.0,
+                              fillColor: Colors.white,
+                              child: Image.asset("images/gym.png",
+                                  height: 50, width: 50),
+                              padding: EdgeInsets.all(15.0),
+                              shape: CircleBorder(),
+                            )),
+                        Column(
+                          children: [
+                            Visibility(
+                                visible: vis,
+                                child: RawMaterialButton(
+                                  onPressed: () {},
+                                  elevation: 2.0,
+                                  fillColor: Colors.white,
+                                  child: Image.asset("images/Blood.png",
+                                      height: 50, width: 50),
+                                  padding: EdgeInsets.all(15.0),
+                                  shape: CircleBorder(),
+                                )),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            RawMaterialButton(
+                              onPressed: () {
+                                setState(() {
+                                  if (vis == true) {
+                                    vis = false;
+                                    var1 = "+";
+                                    check = 2;
+                                  } else {
+                                    vis = true;
+                                    var1 = "-";
+                                    check = 1;
+                                  }
+                                });
+                              },
+                              elevation: 2.0,
+                              fillColor: Colors.white,
+                              child: Text(
+                                "$var1",
+                                style: TextStyle(
+                                    fontSize: 35, color: Color(0XFF4C75D4)),
+                              ),
+                              padding: EdgeInsets.all(15.0),
+                              shape: CircleBorder(),
+                            ),
+                          ],
+                        ),
+                        Visibility(
+                            visible: vis,
+                            child: RawMaterialButton(
+                              onPressed: () {},
+                              elevation: 2.0,
+                              fillColor: Colors.white,
+                              child: Image.asset("images/pizza.png",
+                                  height: 50, width: 50),
+                              padding: EdgeInsets.all(15.0),
+                              shape: CircleBorder(),
+                            ))
+                      ],
+                    ),
+                  ),
                 ],
               ))),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        selectedItemColor: Color(0xFF4C75D4),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings_outlined,
+            ),
+            title: Text('Settings'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(
+              Icons.home_outlined,
+            ),
+            title: new Text(
+              'Home',
+              style: TextStyle(color: Color(0xFF4C75D4)),
+            ),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.medical_services_outlined),
+              title: Text('Medicine'))
+        ],
+      ),
     );
   }
 }
