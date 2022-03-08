a12767cceedfafa5409f61e73ba708142516a69d
1andonlyziz <73785385+1andonlyziz@users.noreply.github.com>
2022-03-08 10:21:56 +0300

    feat: added middleware function to limit the body to only recieve json format from request body

--- Backend/express-app.js
+++ Backend/express-app.js
@@ -3,10 +3,12 @@ const model = require("./model");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json()) // middleware function to limit the body to json format only
+
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
 });
 
 module.exports.app = app;
-
 // Take care of JWT Auth in Middleware
