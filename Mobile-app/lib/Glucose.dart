4759841b64452824ae200bd0933e1e66dadc1f8e
Mubarak <brook.aloufi@gmail.com>
2022-04-02 23:50:07 +0300

    feat: add design to Glucose page

--- Mobile-app/lib/Glucose.dart
+++ Mobile-app/lib/Glucose.dart
@@ -32,14 +32,156 @@ class _GlucoseState extends State<Glucose> {
           color: Color(0XFFF6F8FB),
           width: double.infinity,
           child: Container(
-              margin: EdgeInsets.all(15),
+              margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
               width: double.infinity,
               child: Column(
                 children: [
                   Container(
-                    margin: EdgeInsets.fromLTRB(20, 20, 0, 35),
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 35),
                     alignment: Alignment.centerLeft,
-                  )
+                  ),
+                  Flexible(
+                    flex: 1,
+                    fit: FlexFit.loose,
+                    child: Container(
+                      width: 380,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color(0xFF101E73).withOpacity(0.08),
+                              spreadRadius: 5,
+                              blurRadius: 6,
+                              offset: Offset(1.0, 2.0))
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF4C75D4),
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 30,
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text("106.4 mg/dL",
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.white)),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text("Avg",
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.white)),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text("136.0 mg/dL",
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.white)),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text("Max",
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.white))
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text("66.0 mg/dL",
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.white)),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text("Min",
+                                        style: TextStyle(
+                                            fontSize: 20, color: Colors.white))
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text("est. A1C: 5.34%",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white)),
+                            SizedBox(
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 0),
+                      height: 70,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color(0xFF101E73).withOpacity(0.08),
+                              spreadRadius: 5,
+                              blurRadius: 6,
+                              offset: Offset(1.0, 2.0))
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
+                        alignment: Alignment.centerLeft,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset("images/Blood.png",
+                                    height: 40, width: 40),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "120 mg/dL",
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                SizedBox(
+                                  width: 120,
+                                ),
+                                Column(
+                                  children: [
+                                    Text("Mon, Dec 31"),
+                                    Text(
+                                      "After Snack",
+                                      style: TextStyle(
+                                          fontSize: 12, color: Colors.grey),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               )),
         ),
