0e6baf200347c6e041accb3f87df67b30f58b99b
Mubarak <brook.aloufi@gmail.com>
2022-04-06 09:32:29 +0300

    feat: add design to medicine page

--- Mobile-app/lib/medicine.dart
+++ Mobile-app/lib/medicine.dart
@@ -26,11 +26,69 @@ class _medicineState extends State<medicine> {
           child: Container(
             margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
             width: double.infinity,
-            child: Column(
+            child: ListView(
               children: [
                 Container(
                   margin: EdgeInsets.fromLTRB(20, 0, 0, 35),
                   alignment: Alignment.centerLeft,
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.loose,
+                      child: Container(
+                        width: 380,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color(0xFF101E73).withOpacity(0.08),
+                                spreadRadius: 5,
+                                blurRadius: 6,
+                                offset: Offset(1.0, 2.0))
+                          ],
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+                        child: Container(
+                          margin: EdgeInsets.only(left: 15),
+                          width: double.infinity,
+                          child: Row(
+                            //  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Image.asset("images/Blister.png",
+                                  height: 35, width: 35),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text("Galvus",
+                                  style: TextStyle(
+                                      fontSize: 25, color: Colors.black)),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 6),
+                                child: Text("50 mg",
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.black)),
+                              ),
+                              SizedBox(
+                                width: 115,
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 6),
+                                child: Text("2 a day",
+                                    style: TextStyle(
+                                        fontSize: 17, color: Colors.black)),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
