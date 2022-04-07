76caa252312024a4e128dc41bf29ead5f6435d58
Mubarak <brook.aloufi@gmail.com>
2022-04-07 20:11:36 +0300

    feat: add ResetPassword page

---
+++ Mobile-app/lib/ResetPassword.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ResetPassword extends StatefulWidget {
+  ResetPassword({Key? key}) : super(key: key);
+
+  @override
+  State<ResetPassword> createState() => _ResetPasswordState();
+}
+
+class _ResetPasswordState extends State<ResetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Color(0XFFF6F8FB),
+          width: double.infinity,
+          child: Container(
+            margin: EdgeInsets.all(25),
+            width: double.infinity,
+            child: ListView(
+              children: [],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
