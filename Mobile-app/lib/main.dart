3a983d9bea180e3247749b863027f7e959bcf3d1
Mubarak <brook.aloufi@gmail.com>
2022-04-07 20:19:18 +0300

    feat: add routes for main

--- Mobile-app/lib/main.dart
+++ Mobile-app/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ResetPassword(),
+      home: Home(),
       routes: {
         "BloodGlucose": (context) => BloodGlucose(),
         "Home": (context) => Home(),
