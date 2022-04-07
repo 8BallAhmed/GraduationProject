826184c29422039e23a090f9e684bbdbf1032b52
Mubarak <brook.aloufi@gmail.com>
2022-04-07 19:47:46 +0300

    feat: add design to login page

--- Mobile-app/lib/login.dart
+++ Mobile-app/lib/login.dart
@@ -19,7 +19,155 @@ class _loginState extends State<login> {
             margin: EdgeInsets.all(25),
             width: double.infinity,
             child: ListView(
-              children: [],
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                Image.asset("images/logo.png", height: 80, width: 200),
+                SizedBox(
+                  height: 80,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 70.0,
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 40),
+                        child: TextField(
+                          onChanged: (value) {
+                            //Do something with the user input.
+                          },
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.person_sharp,
+                              color: Colors.grey,
+                            ),
+                            hintText: 'User Name',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 1.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 1.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 40),
+                        child: TextField(
+                          onChanged: (value) {
+                            //Do something with the user input.
+                          },
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.lock_sharp,
+                              color: Colors.grey,
+                            ),
+                            hintText: 'Password',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 1.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 1.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0Xff336CFB),
+                              fontSize: 15),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 3.0,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16.0),
+                        child: Material(
+                          color: Color(0xFF336CFB),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                          elevation: 5.0,
+                          child: MaterialButton(
+                            onPressed: () {
+                              //Implement login functionality.
+                            },
+                            minWidth: 100.0,
+                            height: 40.0,
+                            child: Text(
+                              'Log In',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Don’t have an account?"),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Text(
+                              "Sign up!",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0Xff336CFB),
+                                  fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
