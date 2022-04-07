630c11a6b18694eaa87fb4078bbbba28ac805582
Mubarak <brook.aloufi@gmail.com>
2022-04-07 19:46:47 +0300

    feat: add login page

---
+++ Mobile-app/lib/login.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class login extends StatefulWidget {
+  login({Key? key}) : super(key: key);
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Color(0XFFF6F8FB),
+          width: double.infinity,
+          child: Container(
+            margin: EdgeInsets.all(25),
+            width: double.infinity,
+            child: ListView(
+              children: [],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
